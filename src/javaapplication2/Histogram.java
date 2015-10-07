5e9f0dc8d19d84383370d824d55bd9d26b796fb3
usuario <usuario@10.22.143.7>
2015-10-07 09:48:30 +0100

    P2

--- src/javaapplication2/Histogram.java
+++ src/javaapplication2/Histogram.java
@@ -10,19 +10,19 @@ import java.util.HashMap;
  *
  * @author usuario
  */
-public class Histogram {
-    private final int[] elVector;
+public class Histogram <T>{
+    private final T[] elVector;
     
-    public Histogram(int[] elVector) {
+    public Histogram(T[] elVector) {
         this.elVector = elVector;
     }
 
-    public int[] getElVector() {
+    public T[] getElVector() {
         return elVector;
     }
-    public HashMap <Integer,Integer> getHisto(){
+    public HashMap getHisto(){
        
-        HashMap <Integer,Integer> histo = new HashMap<>();
+        HashMap <T,Integer> histo = new HashMap<>();
         for (int i = 0; i < elVector.length; i++) {
             if(histo.containsKey(elVector[i])){
                 histo.put(elVector[i],0);
